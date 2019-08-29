f0e23f82a4526a4c6165d71f807e6857e82d792e
Daniel-Stafford <stafford.daniel@gmail.com>
2019-08-29 18:11:18 +0300

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,35 @@
+const _ = require('lodash')
+
 const dummy = blogs => {
+	blogs
 	return 1
 }
 
+const totalLikes = blogs => {
+	const reducer = (sum, item) => {
+		return sum + item
+	}
+	console.log('blogs length', blogs.length)
+	return blogs.length === 0
+		? 0
+		: blogs.map(blog => blog.likes).reduce(reducer, 0)
+}
+const favoriteBlog = blogs => {
+	const model = {
+		title: null,
+		author: null,
+		likes: null
+	}
+	if (blogs.length === 0) return []
+
+	let favBlog = _.sortBy(blogs, ['likes']).pop()
+	favBlog = _.pick(favBlog, _.keys(model))
+
+	return favBlog
+}
+
 module.exports = {
-	dummy
+	dummy,
+	totalLikes,
+	favoriteBlog
 }
