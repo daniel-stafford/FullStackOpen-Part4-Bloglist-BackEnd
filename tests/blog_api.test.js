3c3961b44f592572e774a3fc5a9257c774a69d23
Daniel-Stafford <stafford.daniel@gmail.com>
2019-08-30 09:29:18 +0300

    added describe blocks for api test

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -19,59 +19,63 @@ beforeEach(async () => {
 	await blogObject.save()
 })
 
-test('blogs are returned as json', async () => {
-	await api
-		.get('/api/blogs')
-		.expect(200)
-		.expect('Content-Type', /application\/json/)
+describe('addition of a new blog', () => {
+	test('blogs are returned as json', async () => {
+		await api
+			.get('/api/blogs')
+			.expect(200)
+			.expect('Content-Type', /application\/json/)
+	})
+
+	test('all blogs are returned and counted', async () => {
+		const response = await api.get('/api/blogs')
+		expect(response.body.length).toBe(helper.initialBlogs.length)
+	})
+
+	test('unique identifier property of the blog posts is named id', async () => {
+		const response = await api.get('/api/blogs')
+		const idList = response.body.map(blog => blog._id)
+		expect(idList).toBeDefined()
+	})
 })
 
-test('all blogs are returned and counted', async () => {
-	const response = await api.get('/api/blogs')
-	expect(response.body.length).toBe(helper.initialBlogs.length)
-})
+describe('adding a new blog', () => {
+	test('a blog post can be added ', async () => {
+		const newBlog = {
+			title: 'How To Fall from Grace',
+			author: 'Tiger Woods',
+			url: 'www.nike.com'
+		}
 
-test('unique identifier property of the blog posts is named id', async () => {
-	const response = await api.get('/api/blogs')
-	const idList = response.body.map(blog => blog._id)
-	expect(idList).toBeDefined()
-})
+		await api.post('/api/blogs').send(newBlog)
 
-test('a blog post can be added ', async () => {
-	const newBlog = {
-		title: 'How To Fall from Grace',
-		author: 'Tiger Woods',
-		url: 'www.nike.com'
-	}
+		const blogsatEnd = await helper.blogsInDb()
+		expect(blogsatEnd.length).toBe(helper.initialBlogs.length + 1)
+	})
 
-	await api.post('/api/blogs').send(newBlog)
+	test('lilkes property is missing from the request, it will default to the value 0', async () => {
+		const newBlog = {
+			title: 'Does Anybody Like Me',
+			author: 'Richard Simmons',
+			url: 'www.missing.com'
+		}
 
-	const blogsatEnd = await helper.blogsInDb()
-	expect(blogsatEnd.length).toBe(helper.initialBlogs.length + 1)
-})
+		await api.post('/api/blogs').send(newBlog)
+		const blogsatEnd = await helper.blogsInDb()
+		console.log('Blogs at End', blogsatEnd)
+		expect(_.last(blogsatEnd).likes).toBe(0)
+	})
 
-test('lilkes property is missing from the request, it will default to the value 0', async () => {
-	const newBlog = {
-		title: 'Does Anybody Like Me',
-		author: 'Richard Simmons',
-		url: 'www.missing.com'
-	}
-
-	await api.post('/api/blogs').send(newBlog)
-	const blogsatEnd = await helper.blogsInDb()
-	console.log('Blogs at End', blogsatEnd)
-	expect(_.last(blogsatEnd).likes).toBe(0)
-})
-
-test('title and url properties are missing, 400 Bad Request', async () => {
-	const newBlog = {
-		author: 'Richard Simmons',
-		url: 'www.missing.com'
-	}
-	await api
-		.post('/api/blogs')
-		.send(newBlog)
-		.expect(400)
+	test('title and url properties are missing, 400 Bad Request', async () => {
+		const newBlog = {
+			author: 'Richard Simmons',
+			url: 'www.missing.com'
+		}
+		await api
+			.post('/api/blogs')
+			.send(newBlog)
+			.expect(400)
+	})
 })
 
 afterAll(() => {
